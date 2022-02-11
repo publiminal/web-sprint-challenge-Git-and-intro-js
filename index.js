619f9d1d6069211bdc666ea40c0cd4627fdb1cf0
Andres Bernal <publiminal@gmail.com>
2022-02-11 00:24:50 -0500

    done !

--- index.js
+++ index.js
@@ -337,9 +337,12 @@ Use artistInfo to do the following:
 For example artistInfo(artists, 'Frida Kahlo') will return: 
   ["Frida Kahlo de Rivera (Spanish pronunciation: [ˈfɾiða ˈkalo]; born Magdalena Carmen Frida Kahlo y Calderón; 6 July 1907 – 13 July 1954) was a Mexican artist who painted many portraits, self-portraits and works inspired by the nature and artifacts of Mexico. Inspired by the country's popular culture, she employed a naïve folk art style to explore questions of identity, postcolonialism, gender, class and race in Mexican society. Her paintings often had strong autobiographical elements and mixed realism with fantasy. In addition to belonging to the post-revolutionary Mexicayotl movement, which sought to define a Mexican identity, Kahlo has been described as a surrealist or magical realist.Born to a German father and a mestiza mother, Kahlo spent most of her childhood and adult life at her family home in Coyoacán, La Casa Azul, now known and publicly accessible as the Frida Kahlo Museum. She was disabled by polio as a child. Until a traffic accident at age eighteen caused lifelong pain and medical problems, she had been a promising student headed for medical school. During her recovery, she returned to her childhood hobby of art with the idea of becoming an artist."]
 */
-function artistInfo(/*Your Code Here*/){
-  /*Your Code Here*/
+function artistInfo(arr, name){
+  const artist = arr.find(artist => artist.name === name);
+  //console.log('artist', artist);
+  return [artist.bio];
 }
+console.log('Task 8', artistInfo(artists, 'Frida Kahlo'));
 
 
 
@@ -352,10 +355,18 @@ Use artistByCountry to do the following:
 For example artistByCountry(artists, 'Spanish') will return: [ 'Salvador Dali', 'Pablo Picasso', 'Francisco Goya']
 */
 
-function artistByCountry(/*Your Code Here*/){
-  /*Your Code Here*/
+function artistByCountry(arr, nation){
+  //const artByCountry = arr.map(country => country.nationality === nation ? country );
+  const artByCountry = [];
+  for(let i=0; i<arr.length; i++){
+      let artistMatchNation = arr[i].nationality === nation ;
+      if(artistMatchNation){
+        artByCountry.push(arr[i].name);
+      }
+  } 
+  return artByCountry;    
 }
-
+console.log('Task 9', artistByCountry(artists, 'Spanish'));
 
 
 /* ***** END OF TASKS ***** */
